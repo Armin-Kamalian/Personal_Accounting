9468b60d57053468c7c9ad11b21a17905fbd9da1
Armin <arminkamalianceo@gmail.com>
2025-06-29 18:05:09 +0330

    Create Add Transaction & Template

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add_account', views.add_account, name='add_account'),
-    path('add_transaction', views.add_account, name='add_transaction'),
+    path('add_transaction', views.add_transaction, name='add_transaction'),
 ]
--- core/views.py
+++ core/views.py
@@ -29,7 +29,7 @@ def add_account(request):
         with open('db.json', 'r') as file:
             db = json.load(file)
 
-        account_name = request.POST.get('account_name')
+        account_name = request.POST.get('account_name').strip().title()
         account_type = request.POST.get('account_type')
 
         if account_name not in db['accounts']:
@@ -42,3 +42,36 @@ def add_account(request):
             json.dump(db, file, indent=4)
 
     return redirect('/')
+
+
+def add_transaction(request):
+    if request.method == 'POST':
+        with open('db.json', 'r') as file:
+            db = json.load(file)
+
+        debit_account = request.POST.get('debit_account')
+        credit_account = request.POST.get('credit_account')
+        describtion = request.POST.get('describtion')
+        date = request.POST.get('date')
+
+        if debit_account not in db['accounts'] or credit_account not in db['accounts']:
+            return HttpResponse("One of the accounts does not exist!", status=400)
+
+        try:
+            amount = int(request.POST.get('amount'))
+        except (ValueError, TypeError):
+            return HttpResponse("Invalid amount", status=400)
+
+        db['accounts'][debit_account]['debit'].append(amount)
+        db['accounts'][credit_account]['credit'].append(amount)
+
+        db['transactions']['date'].append(date)
+        db['transactions']['debit_account'].append(debit_account)
+        db['transactions']['credit_account'].append(credit_account)
+        db['transactions']['amount'].append(amount)
+        db['transactions']['describtion'].append(describtion)
+
+        with open('db.json', 'w') as file:
+            json.dump(db, file, indent=4)
+
+        return redirect('/')
